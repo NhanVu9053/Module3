4763a20a5719d0b6c17bc4dec7b97e0dbcd94fa4
nhanvu9053 <nhanvu9053@gmail.com>
2020-09-11 14:47:08 +0700

    upppp

---
+++ ToKhaiYTe/ToKhaiYTe/Models/Entinities/UserInformation/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToKhaiYTe.Models.Entinities.UserInformation
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string HoTen { get; set; }
+        public int NamSinh { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string GioiTinh { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string QuocTich { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string HoChieu { get; set; }
+    }
+}
